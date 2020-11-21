0a8fb8f5b85a2b382e13137856bfed968a43baa2
Abhishek M <abhishek.m@so.city>
2020-11-21 18:44:56 +0530

    basic rate limiting to contact query

--- server/controllers/contact.controller.js
+++ server/controllers/contact.controller.js
@@ -1,11 +1,23 @@
 const Router = require('express').Router
+const rateLimit = require('express-rate-limit')
+const MongoStore = require('rate-limit-mongo')
+const secrets = require('../../secrets')
 
 const Queries = require('../models/queries.model')
 const logger = require('../logger')
 const { validateContactMe } = require('../validations')
 const contactRouter = Router()
 
-contactRouter.post('/', async (req, res) => {
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'You are submitting too many contact queries.',
+  store: new MongoStore({
+    uri: secrets.MONGO_URI
+  })
+})
+
+contactRouter.post('/', limiter, async (req, res) => {
   const err = validateContactMe(req.body)
   if (err) {
     logger.error('Validation fail on /contact', { body: req.body, err })
