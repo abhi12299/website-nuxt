6cbf0533a3f4099f373691cdd91434cd240bda15
Abhishek <abhi.9811206369@gmail.com>
2021-01-10 09:00:10 +0530

    add express helmet middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,10 +1,12 @@
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
 
 module.exports = (server) => {
   server.use(bodyParser.json())
   server.use(bodyParser.urlencoded({ extended: true }))
   server.use(cookieParser())
   server.use(compression())
+  server.use(helmet())
 }
