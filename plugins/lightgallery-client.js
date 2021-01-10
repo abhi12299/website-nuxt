8d046d1abac019051264241c7c2d2e35d272aa99
Abhishek <abhi.9811206369@gmail.com>
2021-01-10 09:24:29 +0530

    disable lg-hash plugin for lightgallery

--- plugins/lightgallery-client.js
+++ plugins/lightgallery-client.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import lightgallery from 'lightgallery.js'
 import 'lightgallery.js/dist/css/lightgallery.min.css'
 import 'lg-zoom.js'
-import 'lg-hash.js'
+// import 'lg-hash.js'
 
 Vue.use(lightgallery)
